260d9e3250094341ee6191fba3cb25d12e9d9a9d
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2021-07-28 12:38:11 -0500

    finsished tutorial

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import utilStyles from '../styles/utils.module.css'
+
+
+// pages/404.js
+export default function Custom404() {
+    return  <div>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Bucephala-albeola-010.jpg" // Route of the image file
+                class="center"
+                alt="duck" 
+    /> 
+            <h1>404 - not found - here is a duck</h1> 
+            </div>
+}
